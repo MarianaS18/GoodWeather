940d8e0268ea007c531de968ec31e04344152c55
Mariana <mariana@fastmail.net>
2021-12-25 12:03:15 +0100

    hard coded weather displaying

---
+++ GoodWeather/Cells/WeatherTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherTableViewCell.swift
+//  GoodWeather
+//
+//  Created by Mariana Steblii on 25/12/2021.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+    // MARK: - IBOutlets
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+}
--- GoodWeather/Controllers/WeatherListTableViewController.swift
+++ GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -13,4 +13,25 @@ class WeatherListViewController: UITableViewController {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath) as? WeatherTableViewCell else {
+            fatalError("Weather cell is not found")
+        }
+        cell.cityNameLabel.text = "Solbjerg"
+        cell.temperatureLabel.text = "-2°"
+        return cell
+    }
 }
